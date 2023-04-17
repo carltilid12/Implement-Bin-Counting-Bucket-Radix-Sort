3d6790106fdddff4ce0657f376bd7ad855599fed
Carl Christian Tilid <74176613+carltilid12@users.noreply.github.com>
2023-04-17 21:38:24 +0800

    update

--- tilid_counting_sort.cpp
+++ tilid_counting_sort.cpp
@@ -21,41 +21,46 @@ int main(){
     clock_t time_req;
     int sizearr = 20;
     int arr[sizearr];
-    int bin[sizearr]; //Default Bin sort uses an array that has no duplicates
+    int bin[sizearr];
     int i = 0;
     int num = 0;
 
-    //Making a non duplicating array
+    //Generate Random Array with dupes and setting all bin to 0
     for(i=0; i<sizearr; i++){
-        arr[i] = i;
-    }
-    random_shuffle(&arr[0], &arr[sizearr]);
-    //Display Array and reusing the Bin by assigning Bin to zero
-    for(i=0; i<sizearr; i++){
-        cout << arr[i] << " ";
+        arr[i] = rand() % sizearr;
         bin[i] = 0;
     }
+
+    //Display Array 
+    for(i=0; i<sizearr; i++){
+        cout << arr[i] << " ";
+    }
     cout << endl;
+
     //Start Timer
     time_req = clock();
 
     int index; //Creating Index
-    //Checking arr and marking the bin as 1 if the index is present
+    //Checking arr and adding a count to the buckets to indicate how many values
     for(i=0; i<sizearr; i++){
         index = arr[i];
-        bin[index] = 1;
+        bin[index] = bin[index]+1;
     }
-    //Displaying sorted array by displaying the indexes if Bin
-    for(i=0; i<sizearr; i++){
-        if(bin[i] == 1){
-            arr[i] = i;
+    //Displaying sorted array by displaying the how many times index has been counted
+    i = 0;
+    index = 0;
+
+    while(index < sizearr){
+        //Check if count is greater than 0 then add the values on how many times it counted
+        if(bin[index] > 0){
+            for(int j = 0; j < bin[index]; j++){
+                arr[i] = index;
+                i++; //Move array
+            }
         }
+        index++; //Move Bin
     }
     time_req = clock() - time_req;
-    for(i=0; i<sizearr; i++){
-        cout << arr[i] << " ";
-        bin[i] = 0;
-    }
     cout << endl;
     cout << "The time it took = " << (float)time_req/CLOCKS_PER_SEC << endl;
     return 0;
